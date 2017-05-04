9216e960a6ae0a6608d7c7938dd6dca686280583
PengLunJian <875831103@qq.com>
2017-05-04 19:42:15 +0800

    add search vue component

---
+++ src/components/search/v-search.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="search">
+
+  </section>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: '',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style lang="less" rel="stylesheet/less">
+
+</style>
